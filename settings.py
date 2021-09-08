ba469f36cfb8581ce4404901b676a07381362aa7
Stefan <stefan.eggenreich@gmail.com>
2021-09-08 22:14:24 +0200

    Updated colour palette

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 """
 Definition of settings in the GUI
 """
+import matplotlib.colors as mpl
 
 QC_STATUS_FAIL = 0
 QC_STATUS_PASS = 1
@@ -9,7 +10,13 @@ BTN_PASS_STYLESHEET = "color: black; background-color: green;"
 BTN_FAIL_STYLESHEET = "color: white; background-color: red;"
 DELAY = 100  # msec to wait after pass/fail was clicked
 REQUIRED_TABLE_COLUMNS = ['PatientID', 'imgpath', 'QC', 'comment']
-HIDE_TIMER = True # Hides the "time spent" timer!
-SELECTION_COLOR = {"red": "#FF0000",
-                   "green": "#008000",
-                   "blue": "#0000FF"}
+HIDE_TIMER = True  # Hides the "time spent" timer!
+SELECTION_COLOR = {
+    "coral": "#ff7f50",
+    "navy": "#000080",
+    "orange": "#ffa500",
+    "olive": "#808000",
+    "blue": "#0000FF",
+    "red": "#FF0000",
+    "green": "#008000",
+}
